5e4d684823300852d781d4073e8dfcd33f1e6c90
dylnnlsn <dylnnlsn@gmail.com>
2017-02-03 00:58:36 +0000

    Skeleton outline of resources made

--- main.tf
+++ main.tf
@@ -3,3 +3,41 @@ provider "aws" {
     profile = "${vars.aws_profile}" 
 } 
 
+#IAM 
+#s3_access
+
+#VPC 
+#internet Gateway
+#public Route Table
+#private Route Table
+#Subnets 
+#public 1
+#public 2
+#private 1
+#private 2
+#RDSSubnet1
+#RDSSubnet2
+#RDSSubnet2
+#RDSSubnet3
+
+#Security Groups
+#public
+#private
+#rds
+
+#S3 Code Bucket
+
+#Compute
+#key Pair
+#dev Instance
+    #ansible playbook 
+#elb
+#ami 
+#launch configuration 
+#asg 
+
+#Route53
+#primary zone
+#www
+#dev 
+#database 
